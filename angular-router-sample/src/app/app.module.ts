9ac978134eb37497774e2a9532b04ec2753f0e46
Vi_Nhien <dangvinhien@gmail.com>
2022-02-16 17:20:52 +0700

    .

---
+++ angular-router-sample/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+
+const routes :Routes = ([
+  {path: 'crisis-list', component: CrisisListComponent},
+  {path: 'heroes-list', component: HeroesListComponent},
+  { path: '', redirectTo:'/heroes', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+]);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
